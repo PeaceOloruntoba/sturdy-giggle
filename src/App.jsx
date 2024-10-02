9d4a14417b76b93124387916012a042455950445
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-10-02 20:26:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { rootRoutes } from "./routes/root.routes";
+import { Toaster } from "sonner";
+
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element,
+      children: rootRoutes,
+    },
+  ]);
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Toaster position="top-right" richColors />
+      <RouterProvider router={router} />
+    </>
+  );
 }
