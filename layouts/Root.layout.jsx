7313e82405afa4a7fa15f09a256020b69f1e910e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-10-02 20:29:03 +0100

    Update Root.layout.jsx

--- layouts/Root.layout.jsx
+++ layouts/Root.layout.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Footer from "../src/components/shared/Footer";
+import Navbar from "../src/components/shared/Navbar";
+import { Outlet } from "react-router-dom";
 
 export default function Rootlayout() {
   return (
-    <div>
-      
+    <div className="min-h-screen flex flex-col min-w-screen">
+      <div className="fixed top-0 left-0 py-2 px-6 z-50 bg-black w-full">
+        <Navbar />
+      </div>
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
