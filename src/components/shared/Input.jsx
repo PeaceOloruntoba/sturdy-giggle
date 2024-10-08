ee09c1c2f034b8cdc7a996c9a8cd89ec4fd301e0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-10-08 22:35:52 +0100

    Update Input.jsx

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Input() {
   return (
-    <div>
-      
-    </div>
+    <input type="email" placeholder='Enter your email address' className='outline-none bg-transparent text-xl' />
   )
 }
