942b290c7becb837dc4bcbf3bb24d3c58ecf7865
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-10-26 20:45:17 +0100

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,8 @@
 export { default as logo } from "./images/logo.png";
+export { default as slide1 } from "./images/slide1.png";
+export { default as slide2 } from "./images/slide2.png";
+export { default as slide3 } from "./images/slide3.png";
+export { default as slide4 } from "./images/slide4.png";
+export { default as slide5 } from "./images/slide5.png";
+export { default as slide6 } from "./images/slide6.png";
+export { default as slide7 } from "./images/slide7.png";
