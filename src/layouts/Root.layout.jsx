cd8412dce9d3c6f58717589640beb00b682e2ff6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-10-27 07:40:20 +0100

    Update Root.layout.jsx

--- src/layouts/Root.layout.jsx
+++ src/layouts/Root.layout.jsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom";
 export default function Rootlayout() {
   return (
     <div className="min-h-screen flex flex-col min-w-screen">
-      <div className="fixed top-0 left-0 py-2 px-6 z-50 bg-black w-full">
+      <div className="fixed top-0 left-0 py-2 px-6 z-50 bg-[black] w-full">
         <Navbar />
       </div>
       <div className="w-screen pt-20">
