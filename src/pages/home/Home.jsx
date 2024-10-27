c5e59037f536f07353cff75142038d7894d63802
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-10-27 07:46:36 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import WelcomeMessage from './components/WelcomeMessage'
 
 export default function Home() {
   return (
     <div className='w-full h-full'>
       <Hero />
+      <WelcomeMessage />
     </div>
   )
 }
