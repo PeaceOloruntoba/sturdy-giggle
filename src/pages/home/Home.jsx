91fb97a742c5f50d190909cb845f5b1827437dbd
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-10-27 07:22:52 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='w-full h-full'>
+      <Hero />
     </div>
   )
 }
