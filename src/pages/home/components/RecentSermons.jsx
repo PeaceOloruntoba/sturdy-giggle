e5c2f9e173ba88272d887b5cd2949070c7c029e0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-11-02 02:11:34 +0000

    fff

--- src/pages/home/components/RecentSermons.jsx
+++ src/pages/home/components/RecentSermons.jsx
@@ -5,7 +5,7 @@ export default function RecentSermons() {
   return (
     <div className='w-full flex flex-col px-24'>
       <span>RECENT SERMONS</span>
-      <div className="grid grid-cols-4 gap-8">
+      <div className="grid lg:grid-cols-4 gap-8">
         <SermonCard />
         <SermonCard />
         <SermonCard />
