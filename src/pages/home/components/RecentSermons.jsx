ca19e82555780d58ad5f006a1363432c58b871ee
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-11-03 04:11:34 +0000

    drr

--- src/pages/home/components/RecentSermons.jsx
+++ src/pages/home/components/RecentSermons.jsx
@@ -3,7 +3,7 @@ import SermonCard from '../../../components/container/SermonCard'
 
 export default function RecentSermons() {
   return (
-    <div className='w-full flex flex-col px-24'>
+    <div className='w-full flex flex-col px-12 md:px-24'>
       <span>RECENT SERMONS</span>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-8">
         <SermonCard />
