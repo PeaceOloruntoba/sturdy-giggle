d66281ff700986d376b453e071742b185a2e82a0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-24 07:32:43 +0100

    rm js

--- src/components/shared/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Input({inputClassName, placeholder, type='text'}) {
-  return (
-    <input type={type} placeholder={placeholder} className={inputClassName} />
-  )
-}
